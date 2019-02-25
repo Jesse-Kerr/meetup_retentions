9beaf6c18e130db14d279005e6f6e4361661aeb6
Jesse-Kerr <kerr.jesse@gmail.com>
2019-02-24 22:28:00 -0600

    Removed from draft two many .py files

--- src/move_data_to_pandas.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-import pandas as pd
-from pymongo import MongoClient
-client = MongoClient()
-db = client['meet-ups']
-collection = db['returns_per_group']
-data = pd.DataFrame(list(collection.find()))
